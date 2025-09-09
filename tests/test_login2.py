ad05a82fe51e0ffc3c2bf8510ee69c8de837dcf6
Nguyen Khang <lnkhang112@gmail.com>
2025-09-09 20:57:46 +0700

    add testlogin2.py

---
+++ tests/test_login2.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import pytest
+from time import sleep
+from selenium.webdriver.common.by import By
+from base.base_test import BaseTest
+
+@pytest.mark.usefixtures("setup")
+class TestLogin(BaseTest):
+    
+    def test_valid_login(self):
+        username = self.driver.find_element(By.XPATH,"//input[@name='username']")
+        password = self.driver.find_element(By.XPATH,"//input[@name='password']")
+        loginbt = self.driver.find_element(By.XPATH,"//button[@type='submit']")
+        username.send_keys("Admin")
+        password.send_keys("admin123")
+        loginbt.click()
+
+        sleep(5)
