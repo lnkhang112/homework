c5c1cf68cec80dc822895d37566b35ed75448b89
Nguyen Khang <lnkhang112@gmail.com>
2025-09-10 19:49:37 +0700

    xóa các file không cần thiết

--- bot.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from time import sleep
-
-class Bot:
-    def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get('https://www.google.com')
-        sleep(3)
-        self.driver.quit()
--- test_bot.py
+++
@@ -1,8 +0,0 @@
-from bot import Bot
-
-def test_bot_runs():
-    try:
-        Bot()
-        assert True
-    except Exception as e:
-        assert False, f"Bot failed with error: {e}"
